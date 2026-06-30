63d0f0134bb582034fb899748e118328a068c812
Thanacahi pongsai <jarp12121@gmail.com>
2026-06-30 18:14:06 +0700

    update tb_queries

--- supabase/migrations/20260630023827_rb_queries.sql
+++ supabase/migrations/20260630023827_rb_queries.sql
@@ -1,1 +1,12 @@
-CREATE TABLE 
+ -- ============== start tb_queris
+CREATE TABLE tb_queries (
+    id uuid primary key not null default gen_random_uuid(),
+    title text not null,
+    query text not null,
+    create_by text not null default 'system',
+    create_at datatime timestamp(now()),
+    update_ay text,
+    update_at datatime timestamp(now())
+)
+
+ -- ============== end tb_queris
